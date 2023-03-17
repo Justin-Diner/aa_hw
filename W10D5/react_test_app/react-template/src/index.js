7a89574a13c401ccea02623064996a06867dbe73
Justin <Jcdiner@gmail.com>
2023-03-16 23:21:37 -0400

    react testing

--- W10D5/react_test_app/react-template/src/index.js
+++ W10D5/react_test_app/react-template/src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+};
+
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+	document.getElementById('root'),
 );
 
---
+++ W10D5/second_react_app/testing_react/src/components/Profile.js
@@ -0,0 +1,12 @@
+// ./src/components/Profile.js
+import React from "react";
+import { useParams } from 'react-router-dom';
+
+const Profile = () => {
+  const params = useParams();
+  const { userId } = params;
+
+  return <h1>Hello from User Profile {userId}!</h1>
+}
+
+export default Profile;
